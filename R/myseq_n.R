3d889e06499bac42dffcff867a11e9b19d348836
eqdietze <ed0998a@student.american.edu>
2021-02-24 17:17:15 -0500

    Info on myseq_n

--- R/myseq_n.R
+++ R/myseq_n.R
@@ -1,3 +1,14 @@
+#' Recursive Sequence of x and n
+#'
+#' This function takes as input a vector x containing the first three numeric elements of this sequence and a positive integer n denoting the final element of the sequence to calculate and returns element n.
+#'
+#' @param x integer
+#' @param n integer
+#'
+#' @return an integer n
+#' @export myseq_n
+#'
+#' @examples myseq_n(x = c(2, 3, 3), n = 3) myseq_n(x = c(2, 4, 3), n = 4)
 myseq_n <- function(x, n) {
   stopifnot(length(x) == 3 & is.numeric(x))
   stopifnot(n > 0 & as.integer(n))
